aa110409f1fc0ab7fe803829c296f01feeb3542a
sedin <leesejinn@naver.com>
2019-12-02 23:16:50 +0900

    2단계 시작 부분 및 구현 할 함수 선언
    메뉴선택 부분과 선택하고 나면 호출 할 함수 선언
    예외처리 잘 못 선택하면 다시 선택하라고 메세지 출력

--- Baseball2.py
+++ Baseball2.py
@@ -1,5 +1,20 @@
 
+def write_File():
+    return 1
+def open_File():
+    return 1
+WW
 print("신나는 야구시합")
 print("1. 데이터 입력")
 print("2. 데이터 출력")
 print()
+
+num = int(input("메뉴선택 (1-2) "))
+
+if num == 1:
+    write_File()
+elif num == 2:
+    open_File()
+else:
+    print("1 or 2만 선택 해주세요")
+    
