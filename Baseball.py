d25fb26a83cfa27423e2772374fa11002ca4f11b
sedin <leesejinn@naver.com>
2019-12-02 21:29:15 +0900

    def play_Check(action_list, check): 수정 및 def play_Finish(action_list): 구현
    1단계 요구사항의 (8)부분 3아웃 시 전체 안타수 출력 후 게임 종료를 위해 play_Finish()함수 구현
    play_Check()함수에서 3스트라이크 일 때 아웃카운트 1 증가 후 아웃카운트가 3이면 경기 종료 추가 구현
    테스트를 위해 while문을 넣고 스트라이크, 볼 판정 이후 아웃이나 안타가 없을 때 (play_check == 0 or play_check ==1)
    스트라이크, 볼, 안타, 아웃 판정 이후 경기가 안 끝났을 때 (play_check == 2 or play_check == 3)
    3아웃 시 경기 종료 (play_check == 4) play_Finish()함수 호출 후 경기 종료

--- Baseball.py
+++ Baseball.py
@@ -3,6 +3,7 @@ import random
 def game_Start():
     print("신나는 야구 게임!")
     print("첫 번째 타자가 타석에 입장했습니다.")
+    print()
 
 def play_Game(action_list):
     action = random.randrange(0, 4)
@@ -23,10 +24,13 @@ def play_Check(action_list, check):
         print("스트라이크!")
         return 0
     elif check == 0 and action_list[0] == 3:
-        print("스트라이크!")
-        print("아웃! 다음 타자가 타석에 입장했습니다.")     #다음 타자 입장
+        print("스트라이크!")        
         action_list[3] += 1                                 #3스트 1아웃 추가
-        return 3
+        if action_list[3] < 3:                              #아웃 3개 미만 일 때    
+            print("아웃! 다음 타자가 타석에 입장했습니다.") #다음 타자 입장
+            return 3
+        else:
+            return 4                                        #3아웃 일 때 
     elif check == 1 and action_list[1] < 4:
         print("볼!")
         return 1
@@ -37,30 +41,49 @@ def play_Check(action_list, check):
         return 2 
     elif check == 2:
         print("안타! 다음 타자가 타석에 입장했습니다.")
-    elif check == 3:
+        return 2
+    elif check == 3 and action_list[3] < 3:
         print("아웃! 다음 타자가 타석에 입장했습니다.")
+        return 3
+    elif check == 3 and action_list[3] == 3:
+        print("아웃!")
+        return 4                                            #3아웃 일 때
 
 def play_Show(action_list):
-    print(str(action_list[0]) + "S " + str(action_list[1]) + "B " + str(action_list[2]) + "H " + str(action_list[3]) + "Out ")
+    print(str(action_list[0]) + "S " + str(action_list[1]) + "B " + str(action_list[3]) + "Out ")
     print("")
 
 def play_Init(action_list):
     action_list[0] = 0
     action_list[1] = 0
+
+def play_Finish(action_list):
+    print("최종 안타수: " + str(action_list[2]))
+    print("GAME OVER")
     
-glist = [0, 0, 0, 0] # 스트 / 볼 / 안타 / 아웃 카운트 하는 list
+    
+glist = [0, 0, 0, 0]                                        # 스트 / 볼 / 안타 / 아웃 카운트 하는 list
 random_check = 0
 play_check = 0
 
+game_Start()                                                # 게임 시작
 
-game_Start()
+while(True):
+    
+    random_check = play_Game(glist)                         # 0,1,2,3 -> 스트라이크 / 볼 / 안타/ 아웃 -> 체크에 넣음
+    play_check = play_Check(glist, random_check)            # 3스트 4볼 안타 3아웃 체크
+    
+    if play_check == 0 or play_check == 1:                  # 스트, 볼 나왔는데 3스트, 4볼 미만 일 때
+        play_Show(glist)
+    elif play_check == 2 or play_check == 3:                # 스트, 볼 나왔는데 3스트, 4볼 일 때
+        play_Init(glist)
+        play_Show(glist)
+    elif play_check == 4:                                   # 3아웃 일 때 게임 종료
+        play_Init(glist)
+        play_Show(glist)
+        play_Finish(glist)
+        break
 
-# 0,1,2,3 -> 스트라이크 / 볼 / 안타/ 아웃 -> 체크에 넣음
-random_check = play_Game(glist)
-
-#랜덤으로 나온것 출력
-play_check = play_Check(glist, random_check)
-
-play_Show(glist)
+    
 
 
