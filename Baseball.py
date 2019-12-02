d7053b6331b3638eb7e6711b5f308e4548954ded
sedin <leesejinn@naver.com>
2019-12-02 19:36:23 +0900

    def play_Game(action_list): 구현
    1단계 요구사항의 (2)부분 랜덤으로 스트라이크 / 볼 / 안타 / 아웃 중 1개 값 반환

--- Baseball.py
+++ Baseball.py
@@ -1,5 +1,25 @@
+import random
+
 def game_Start():
     print("신나는 야구 게임!")
     print("첫 번째 타자가 타석에 입장했습니다.")
+
+def play_Game(action_list):
+    action = random.randrange(0, 4)
     
+    if action == 0:
+        action_list[action] += 1
+    elif action == 1:
+        action_list[action] += 1
+    elif action == 2:
+        action_list[action] += 1
+    elif action == 3:
+        action_list[action] += 1
+        
+    return action
+
+glist = [0, 0, 0, 0]
+
 game_Start()
+play_Game(glist)
+
